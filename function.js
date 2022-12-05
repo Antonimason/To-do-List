b68324fca652f6c9cbd9b36547afba817265c69f
Antonimason <elchuli3000@gmail.com>
2022-12-05 22:30:43 +0000

    arreglo

--- function.js
+++ function.js
@@ -53,15 +53,15 @@ function show() {
 
         tasksView.innerHTML += 
         `
-                <div class="row border-bottom">
-                    <div class="col-3 text-center">
-                        <h5 class="text-center my-3">${title}</h5>
+                <div class="row border-bottom cuadro">
+                    <div class="col-4 text-center">
+                        <b><p class="text-center my-3">${title}</p></b>
                     </div>
                     <div class="col-6 text-center">
                         <p class="text-center my-3">${description}</p>
                     </div>
-                    <div class="col-3 text-center">
-                    <a href="" onclick="Delete('${title}')"><i class="fa-sharp fa-solid fa-trash display-6 my-2 text-danger"></i></a>
+                    <div class="col-2 text-center">
+                        <i onclick="Delete('${title}')" class="fa-sharp fa-solid fa-trash display-6 my-3 text-danger vergacion"></i>
                     </div>
                 </div>
         `
